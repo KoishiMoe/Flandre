0891303a9a0b819e51050a2705f84df7684c6265
KoishiStudio <68314080+KoishiStudio@users.noreply.github.com>
2021-10-31 11:45:06 +0800

    wiki

--- bot.py
+++ bot.py
@@ -6,12 +6,12 @@ from nonebot.adapters.cqhttp import Bot as Flandre
 
 # Custom your logger
 # 
-# from nonebot.log import logger, default_format
-# logger.add("error.log",
-#            rotation="00:00",
-#            diagnose=False,
-#            level="ERROR",
-#            format=default_format)
+from nonebot.log import logger, default_format
+logger.add("error.log",
+           rotation="00:00",
+           diagnose=False,
+           level="ERROR",
+           format=default_format)
 
 # You can pass some keyword args config to init function
 nonebot.init()
@@ -20,9 +20,10 @@ app = nonebot.get_asgi()
 driver = nonebot.get_driver()
 driver.register_adapter("cqhttp", Flandre)
 
-config = driver.config
-
 nonebot.load_builtin_plugins()
+
+# 测试用
+nonebot.load_plugin("nonebot_plugin_test")
 
 # Please DO NOT modify this file unless you know what you are doing!
 # As an alternative, you should use command `nb` or modify `pyproject.toml` to load plugins
@@ -30,7 +31,7 @@ nonebot.load_from_toml("pyproject.toml")
 
 # Modify some config / config depends on loaded configs
 # 
-# config = driver.config
+config = driver.config
 # do something...
 
 
---
+++ src/plugins/mediaWiki/__init__.py
@@ -0,0 +1,33 @@
+import re
+
+from nonebot.adapters.cqhttp import Bot, Message, MessageEvent
+from nonebot.typing import T_State
+from nonebot import on_regex, on_command
+
+from urllib import parse
+
+ARTICLE = r"\[\[(.*?)\]\]"
+TEMPLATE = r"\{\{(.*?)\}\}"
+
+# TODO:将wiki地址存入配置文件中
+WIKIURL = "https://zh.wikipedia.org/wiki/"
+
+wiki = on_regex(ARTICLE)
+wiki_template = on_regex(TEMPLATE)
+
+# FIXME:消息被错误转换为转义字符导致无法匹配
+@wiki.handle()
+async def _wiki(bot: Bot, event: MessageEvent):
+    msg = str(event.message).strip()
+    title = re.findall(ARTICLE, msg)[0]
+    url = WIKIURL + parse.quote(title)
+
+    await bot.send(event, url)
+
+@wiki_template.handle()
+async def _wiki_template(bot: Bot, event: MessageEvent):
+    msg = str(event.message).strip()
+    title = re.findall(TEMPLATE, msg)[0]
+    url = WIKIURL + "Template:" +parse.quote(title)
+
+    await bot.send(event, url)
