02d35767aa0ab30d28d6749c3c7157a97f208325
KoishiChan <68314080+KoishiStudio@users.noreply.github.com>
2021-11-15 18:26:15 +0800

    feat: 增加struct_message处理

--- src/plugins/anti_miniapp/__init__.py
+++ src/plugins/anti_miniapp/__init__.py
@@ -5,9 +5,9 @@ from json import JSONDecodeError
 
 from nonebot.typing import T_State
 from nonebot.adapters.cqhttp import Bot, MessageEvent, unescape
-from nonebot import on_keyword
+from nonebot import on_regex
 
-anti_miniapp = on_keyword('com.tencent.miniapp')
+anti_miniapp = on_regex('com.tencent.miniapp')
 
 
 @anti_miniapp.handle()
@@ -22,6 +22,32 @@ async def _anti_miniapp(bot: Bot, event: MessageEvent, state: T_State):
         data: dict = json.loads(msg)
         url = data.get("meta", None).get("detail_1", None).get("qqdocurl", None)
         # 因为url键对应的链接只能打开小程序，所以提取qqdocurl作为小程序链接
+        if url:
+            await bot.send(event, message=url)
+        else:
+            raise AttributeError  # 偷懒共用下AttributeError的错误提示23333
+    except KeyError as e:
+        await bot.send(event, message="解析失败：不是合法的小程序")
+    except JSONDecodeError as e:
+        await bot.send(event, message="解析失败：无法找到有效的json字段")
+    except AttributeError as e:
+        await bot.send(event, message="解析失败：无法找到有效的链接")
+
+
+anti_structmsg = on_regex('com.tencent.structmsg')
+
+
+@anti_structmsg.handle()
+async def _anti_structmsg(bot: Bot, event: MessageEvent, state: T_State):
+    msg = str(event.message).strip()
+    try:
+        msg = re.findall(r"\{.*(?=\})\}", msg)[0]
+        msg = unescape(msg)
+        data: dict = json.loads(msg)
+        meta = data.get("meta", None)
+        data2 = meta.get(list(meta.keys())[0], None)
+        url = data2.get("jumpUrl", None)
+        # 目前样本比较少，不确定jumpUrl是不是一定是跳转链接，如有问题请提issue
         if url:
             await bot.send(event, message=url)
         else:
