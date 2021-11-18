b9ade245f5a13be21f1a20c1b6a6818a93cb4189
KoishiChan <68314080+KoishiStudio@users.noreply.github.com>
2021-11-18 22:12:03 +0800

    feat(config): 新增自动更新配置文件、读取配置文件失败时的异常处理

---
+++ src/utils/config/init_config.py
@@ -0,0 +1,59 @@
+import copy
+from pathlib import Path
+
+from yaml import safe_load, safe_dump
+
+DEFAULT_CONFIG = {
+    'BotConfig':
+        {
+            'port': 8080,
+            'debug': False,
+            'superusers': [""],
+            'nickname': ["芙兰", "芙兰朵露", "芙兰朵露·斯卡蕾特", "Flandre"],
+            'command_start': ["", "/"],
+            'command_sep': ["."],
+            'session_expire_timeout': 60,
+        },
+
+    'WithdrawConfig':
+        {
+            'max_withdraw_num': 50,
+        },
+
+}
+
+
+class Initcfg:
+    @staticmethod
+    def new_config(path: Path):
+        try:
+            with open(path, 'w', encoding='utf-8') as w:
+                w.write(safe_dump(DEFAULT_CONFIG).encode('utf-8').decode('unicode-escape'))  # 防止中文被输出为转义字符
+            return True
+        except PermissionError as e:
+            return "初始化配置文件错误：没有写权限"
+        except IsADirectoryError as e:
+            return "初始化配置文件错误：已有名为'config.yaml'的目录存在，请删除或重命名该目录"
+        except Exception as e:
+            return f"初始化配置文件错误：未知错误\n{e}"
+
+    @staticmethod
+    def update_config(path: Path):
+        try:
+            with open(path, 'r', encoding='utf-8') as f:
+                conf: dict = safe_load(f)
+                new_conf = copy.deepcopy(conf)
+            with open(path, 'w', encoding='utf-8') as f:
+                for i in DEFAULT_CONFIG.keys():
+                    if conf.get(i, {}):
+                        for j in DEFAULT_CONFIG.get(i, {}).keys():
+                            if not conf[i].get(j, None):
+                                new_conf[i][j] = DEFAULT_CONFIG[i][j]
+                    else:
+                        new_conf[i] = DEFAULT_CONFIG[i]
+                f.write(safe_dump(new_conf).encode('utf-8').decode('unicode-escape'), )
+            return True
+        except PermissionError as e:
+            return "更新配置文件错误：没有写权限"
+        except Exception as e:
+            return f"更新配置文件错误：未知错误\n{e}"
