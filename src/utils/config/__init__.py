29fc19bca8cdec1b0c690555d2e4dc4af1adb8a3
KoishiStudio <68314080+KoishiStudio@users.noreply.github.com>
2021-11-18 20:10:04 +0800

    feat(config): 移除默认配置文件，改为首次启动时生成

--- src/utils/config/__init__.py
+++ src/utils/config/__init__.py
@@ -1,8 +1,31 @@
 from pathlib import Path
 from datetime import timedelta
-from yaml import safe_load
+from yaml import safe_load, safe_dump
 
 CONFIG_PATH = Path(".") / "config.yaml"
+DEFAULT_CONFIG = {
+    'BotConfig':
+        {
+            'port': 8080,
+            'debug': False,
+            'superusers': [""],
+            'nickname': ["芙兰", "芙兰朵露", "芙兰朵露·斯卡蕾特", "Flandre"],
+            'command_start': ["", "/"],
+            'command_sep': ["."],
+            'session_expire_timeout': 60,
+        },
+
+    'WithdrawConfig':
+        {
+            'max_withdraw_num': 50,
+        },
+
+}
+if not CONFIG_PATH.is_file():
+    with open(CONFIG_PATH, 'w', encoding='utf-8') as w:
+        w.write(safe_dump(DEFAULT_CONFIG).encode('utf-8').decode('unicode-escape'))  # 防止中文被输出为转义字符
+    print("未找到配置文件，已在bot所在目录生成config.yaml,请参考文档进行修改后再次启动bot")
+    exit(0)
 with open(CONFIG_PATH, 'r', encoding='utf-8') as f:
     config = safe_load(f)
 
