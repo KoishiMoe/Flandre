d324a7f768fbeb89964594c2ca97ea965fa49116
KoishiChan <68314080+KoishiStudio@users.noreply.github.com>
2021-11-19 14:34:54 +0800

    feat(pixiv): 群聊支持多图合并转发，防刷屏
    已知问题：图太多时大概率超时，不影响发送，影响撤回

--- src/plugins/pixiv/__init__.py
+++ src/plugins/pixiv/__init__.py
@@ -1,10 +1,11 @@
 import asyncio
 import re
 
-from nonebot.adapters.cqhttp import Bot, MessageEvent, MessageSegment, Message
 from nonebot import on_command
+from nonebot.adapters.cqhttp import Bot, MessageEvent, Message, GroupMessageEvent
 from nonebot.typing import T_State
 
+from src.utils.config import BotConfig, PixivConfig
 from .data_source import Pixiv
 
 get_pixiv = on_command("#pixiv", aliases={"#Pixiv", "#P站", "p站", "#p站图", "#P站图"})
@@ -25,6 +26,22 @@ async def _get_pixiv(bot: Bot, event: MessageEvent, state: T_State):
     images = await Pixiv.get_pic(msg)
     if not images:
         await bot.send(event, message=Message("没有找到该图片的说……"))
-    for image in images:
-        await bot.send(event, Message(image))
-        await asyncio.sleep(3)
+    elif len(images) > 5 and PixivConfig.use_forward_msg and isinstance(event, GroupMessageEvent):  # 似乎私聊不支持合并转发
+        messages = await _fake_msg(images, bot.self_id)
+        await bot.send_group_forward_msg(group_id=event.group_id, messages=messages)
+    else:
+        for image in images:
+            await bot.send(event, Message(image))
+            await asyncio.sleep(3)
+
+
+async def _fake_msg(images: list, qq: str):
+    node = list()
+
+    for i in images:
+        name = list(BotConfig.nickname)[0] if BotConfig.nickname else '芙兰'
+        content = i
+        dic = {"type": "node", "data": {"name": name, "uin": qq, "content": content}}
+        node.append(dic)
+
+    return node
