44ab0910384822088fdc42519167cd1e1d9743e0
KoishiChan <68314080+KoishiStudio@users.noreply.github.com>
2021-12-04 01:17:31 +0800

    test(mediawiki): 检查条目名中是否有本机ip（因没什么用暂时废弃）

--- src/plugins/wiki/data_source.py
+++ src/plugins/wiki/data_source.py
@@ -1,6 +1,8 @@
+# import re
 from urllib import parse
 
-# from mediawiki import MediaWiki
+# from aiohttp import ClientSession
+
 from .mediawiki import MediaWiki
 
 
@@ -21,17 +23,28 @@ class Wiki:
         try:
             if self.__api_url == '':
                 raise NoApiUrlException
-            # self.__api = MediaWiki(url=self.__api_url)
-            # result = self.__api.opensearch(title, results=1)
             result = await MediaWiki.opensearch(self.__api_url, title, results=1)
         except:  # 针对无法正常调用API的情况的回落，例如WAF
             result = await self.url_parse(title)
             result = f"由条目名直接生成的链接：\n{result}"
             return result
         if result:
+            # title0 = title  # 后续检查ip要用原始title,先备份下……（历史遗留问题）
             title = result[0][0]
             url = result[0][2]
             result = f"标题：{title}\n链接：{url}"
+
+            # 小丑了，opensearch api并不会直接将这些特殊页面转换成具体的用户页地址，不过出于以后可能有用的原因暂时保留
+            #
+            # # 检查条目名中是否有自己的公网ip,防止恶意用户用特殊页面搞事
+            # if re.match(r"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$",
+            #             title):
+            #     if not self.check_ip(title, 4):
+            #         result = await self.url_parse(title0)
+            # if re.match(r"^(?:[A-F0-9]{1,4}:){7}[A-F0-9]{1,4}$", title, re.I):
+            #     if not self.check_ip(title, 6):
+            #         result = await self.url_parse(title0)
+
         else:
             result = await self.url_parse(title)
             result = f"没有找到该条目，以下是由条目名直接生成的链接\n{result}"
@@ -40,3 +53,20 @@ class Wiki:
     async def url_parse(self, title: str) -> str:
         result = f"标题：{title}\n链接：{self.__url}/{parse.quote(title)}"
         return result
+
+    # 暂时无用的ip检查代码
+    # @staticmethod
+    # async def check_ip(title: str, ipv: int):
+    #     try:
+    #         async with ClientSession() as session:
+    #             if ipv == 4:
+    #                 resp = await session.get('https://v4.ident.me/')
+    #             else:
+    #                 resp = await session.get('https://v6.ident.me/')
+    #             ip = resp.text()
+    #         if ip in title:
+    #             return False
+    #         else:
+    #             return True
+    #     except:
+    #         return False
