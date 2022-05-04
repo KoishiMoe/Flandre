ad2f3e367a665a46aeeb8114c24e7eab140b00f2
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2022-05-04 19:27:18 +0800

    fix(sevice): 修复事件无上下文时，匹配rule出错的Bug

--- src/plugins/service/rule.py
+++ src/plugins/service/rule.py
@@ -15,21 +15,25 @@ def online(service: str) -> Rule:
             and await get_status(service, "global")
         ):
             return False
-        if await GROUP(bot, event):
-            # 群消息类事件比较多，考虑后续兼容性问题，这里用权限检测
-            if not (
-                await get_status(service, "g*")
-                and await get_status("*", "g" + str(event.group_id))
-                and await get_status(service, "g" + str(event.group_id))
-                and await get_status("*", f"g{str(event.group_id)}u{str(event.get_user_id())}")
-                and await get_status(service, f"g{str(event.group_id)}u{str(event.get_user_id())}")
-            ):
+        try:
+            if await GROUP(bot, event):
+                # 群消息类事件比较多，考虑后续兼容性问题，这里用权限检测
+                if not (
+                    await get_status(service, "g*")
+                    and await get_status("*", "g" + str(event.group_id))
+                    and await get_status(service, "g" + str(event.group_id))
+                    and await get_status("*", f"g{str(event.group_id)}u{str(event.get_user_id())}")
+                    and await get_status(service, f"g{str(event.group_id)}u{str(event.get_user_id())}")
+                ):
+                    return False
+            elif await PRIVATE(bot, event) and not await get_status(service, "u*"):
                 return False
-        elif await PRIVATE(bot, event) and not await get_status(service, "u*"):
-            return False
-        return (
-                await get_status(service, "u" + str(event.get_user_id()))
-                and await get_status("*", "u" + str(event.get_user_id()))
-        )
+
+            return (
+                    await get_status(service, "u" + str(event.get_user_id()))
+                    and await get_status("*", "u" + str(event.get_user_id()))
+            )
+        except ValueError:
+            return False  # 事件没有上下文时，获取user_id可能会抛出ValueError
 
     return Rule(_is_online)
