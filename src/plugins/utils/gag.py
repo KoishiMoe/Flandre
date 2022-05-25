2a49aaa7411a6419a4233b62c5fd0e2fd3e5a071
KoishiMoe <68314080+KoishiMoe@users.noreply.github.com>
2022-05-25 13:30:31 +0800

    fix(utils): 增加bot连接时禁言检测，以解决bot重连或重启时禁言记录丢失的问题 (#35)

--- src/plugins/utils/gag.py
+++ src/plugins/utils/gag.py
@@ -1,6 +1,6 @@
 from time import time
 
-from nonebot import on_notice
+from nonebot import on_notice, get_driver
 from nonebot.adapters.onebot.v11 import Bot, Event, GroupBanNoticeEvent
 from nonebot.adapters.onebot.v11.permission import GROUP
 from nonebot.log import logger
@@ -9,6 +9,40 @@ from nonebot.rule import Rule
 from src.utils.config import BotConfig
 
 record = {}
+
+driver = get_driver()
+
+
+@driver.on_bot_connect
+async def _check_existing_gag(bot: Bot):
+    global record
+
+    groups: list[dict] = []
+    for i in range(3):
+        try:
+            groups = await bot.get_group_list()
+            break
+        except Exception as e:
+            logger.warning(f"获取群列表出错，正在重试（{i + 1}/3）……\n错误信息：{e}")
+
+    if not groups:
+        return
+
+    record = {}
+    for group in groups:
+        gid = group.get("group_id")
+        if gid:
+            try:
+                bot_info = await bot.get_group_member_info(group_id=gid, user_id=int(bot.self_id))
+                t = time()
+                if bot_info.get("shut_up_timestamp"):
+                    record[gid] = {
+                        "time": t,
+                        "duration": bot_info["shut_up_timestamp"] - t,
+                    }
+            except Exception as e:
+                logger.warning(f"获取群{group}的禁言状态时发生了错误：{e}")
+
 
 gag = on_notice(block=False)
 
